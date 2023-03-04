b0760e1a3a1d8c041ae102568efe48e9ab27011d
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-04 12:29:10 +0500

    Added Project Files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/**typing animation */
+var typed = new Typed(".typing", {
+    strings:["", "Web Desinger", "Web Developer", "Graphic Designer", "Creative Writer", "Transcriber", "Learner", "Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true 
+})
